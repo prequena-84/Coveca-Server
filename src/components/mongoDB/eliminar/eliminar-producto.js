41c48106b0f3e7b6968b938754b4366cca7a0623
prequena-84 <pedrorequenarondon@hotmail.com>
2025-03-09 22:37:56 -0400

    server-13

--- src/components/mongoDB/eliminar/eliminar-producto.js
+++ src/components/mongoDB/eliminar/eliminar-producto.js
@@ -2,7 +2,7 @@ const Producto = require('../insertar/producto');
 
 DeleteProduct();
 
-// funciono este metodo para eliminar el prodcutos seleccionado
+// funciono este metodo para eliminar el prodcutos seleccionado pero tengo que quitar el codigo que carga la funcion.
 async function DeleteProduct() {
     await Producto.deleteOne({Id_Producto:'001-000001'})
     .then( resultado => console.log( resultado ))
